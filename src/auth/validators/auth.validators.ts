3bed420df9d5e894cedaf126a93f73eca9fd5713
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-08-20 22:38:27 +0530

    feat(auth/validators): implement zod validators for register, verifyEmail, and login requests

--- src/auth/validators/auth.validators.ts
+++ src/auth/validators/auth.validators.ts
@@ -1,4 +1,30 @@
-const registerValidator={}
-const loginValidator={}
+import { email, z } from "zod";
+const registerValidator = {
+  body: z.object({
+    name: z.string().min(3).max(20),
+    email: z.email(),
+    password: z
+      .string()
+      .min(8, { error: "Password must be at least 8 characters long" })
+      .max(16, { error: "Password must be at most 16 characters long" })
+      .regex(
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$/,
+        {
+          error:
+            "Password must contain at least one uppercase letter, one lowercase letter, number and one special character.",
+        }
+      ),
+  }),
+};
 
-export {registerValidator,loginValidator}
+const verifyEmailValidator = {
+  params: z.object({
+    token: z.string(),
+  }),
+};
+
+const loginValidator = {
+  body: registerValidator.body.pick({ email: true, password: true }),
+};
+
+export { registerValidator, verifyEmailValidator, loginValidator };
