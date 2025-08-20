ea1f60b51d781d369c9b3ceaa5af4542f46a1acc
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-08-20 22:28:06 +0530

    feat(app): add routers for admin, auth, categories, and posts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,10 @@
 import express, { Express } from "express";
 import cookieParser from "cookie-parser";
 import errorMiddleware from "../middlewares/error.middlewares";
-
+import AdminRouter from "../admin/routes/admin.routes";
+import AuthRouter from "../auth/routes/auth.routes";
+import CategoriesRouter from "../categories/routes/category.routes";
+import PostsRouter from "../posts/routes/post.routes";
 function app(): Express {
   const app: Express = express();
 
@@ -9,13 +12,10 @@ function app(): Express {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
-  app.get("/", (req, res) => {
-    res.json({
-      headers: req.headers,
-      cookies: req.cookies,
-    });
-  });
-
+  app.use("/api/v1/admin", AdminRouter);
+  app.use("/api/v1/auth", AuthRouter);
+  app.use("/api/v1/categories", CategoriesRouter);
+  app.use("/api/v1/posts", PostsRouter);
   app.use(errorMiddleware);
   return app;
 }
