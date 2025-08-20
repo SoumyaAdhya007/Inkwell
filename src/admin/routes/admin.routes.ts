66073b197ec99ba1337b68c0b584dede62233b0e
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-08-20 22:54:40 +0530

    feat(admin/routes): add routes for getAllPendingPosts(pending), approvePostById and rejectPostById

---
+++ src/admin/routes/admin.routes.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import validateReq from "../../middlewares/validationRequest.middlewares";
+
+import authCheck from "../../middlewares/auth.middlewares";
+import adminCheck from "../../middlewares/admin.middleware";
+import apiKeyCheck from "../../middlewares/apiKey.middlewares";
+import {
+  approvePostByIdValidator,
+  rejectPostByIdValidator,
+} from "../validators/admin.validators";
+import {
+  approvePostById,
+  getAllPendingPosts,
+  rejectPostById,
+} from "../controllers/admin.controllers";
+
+const router = Router();
+router.use(authCheck, adminCheck, apiKeyCheck);
+
+router.route("/posts").get(getAllPendingPosts);
+
+router
+  .route("/posts/:id/approve")
+  .put(validateReq(approvePostByIdValidator), approvePostById);
+
+router
+  .route("/posts/:id/reject")
+  .put(validateReq(rejectPostByIdValidator), rejectPostById);
+
+export default router;
