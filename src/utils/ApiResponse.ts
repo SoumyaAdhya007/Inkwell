ce8eb87e55b6b63596e4f925e5b286a3d51fb468
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-08-20 23:27:02 +0530

    fix(utils/ApiResponse): fix constructor

--- src/utils/ApiResponse.ts
+++ src/utils/ApiResponse.ts
@@ -1,11 +1,9 @@
-import { da } from "zod/v4/locales/index.cjs";
-
 class ApiResponse {
   statusCode?: number;
   success?: boolean;
   message?: string;
   data?: any;
-  constructor(statusCode: number, message: string = "Success", data: any) {
+  constructor(statusCode: number, message: string = "Success", data: any = []) {
     (this.statusCode = statusCode),
       (this.success = statusCode < 400),
       (this.message = message),
