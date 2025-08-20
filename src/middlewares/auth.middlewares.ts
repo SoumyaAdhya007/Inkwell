f2d52e0c9bcfbfc864d028beed83ff832e6dfab1
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-08-20 23:07:17 +0530

    fix(middlewares/auth): resolve issues in authentication middleware

--- src/middlewares/auth.middlewares.ts
+++ src/middlewares/auth.middlewares.ts
@@ -4,19 +4,13 @@ import ApiError from "../utils/ApiError";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import env from "../config/env.config";
 import User from "../models/user.models";
-
+import { decodedUser } from "../types/express";
+interface DecodeToken extends JwtPayload {
+  id: string;
+  role: string;
+}
 const authCheck = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    interface Decode extends JwtPayload {
-      id: string;
-      role: string;
-    }
-    interface User {
-      name: string;
-      email: string;
-      role: string;
-      isVerified: boolean;
-    }
     const accessToken: string =
       req.cookies.accessToken ||
       req.headers.authorization?.split(" ")[1] ||
@@ -24,17 +18,21 @@ const authCheck = asyncHandler(
     if (!accessToken) {
       throw new ApiError(401, "Unauthorized - Invalid access token.");
     }
-    const decode = jwt.verify(accessToken, env.ACCESS_TOKEN_SECRET) as Decode;
-    if (!decode) {
-      throw new ApiError(401, "Unauthorized - Invalid access token.");
+
+    let decoded: DecodeToken;
+    try {
+      decoded = jwt.verify(accessToken, env.ACCESS_TOKEN_SECRET) as DecodeToken;
+    } catch {
+      throw new ApiError(401, "Unauthorized - Invalid API Key");
     }
-    const user = await User.findById(decode.id).select(
+  
+    const user = await User.findById(decoded.id).select(
       "-password -verificationToken -refreshToken -resetPasswordToken -verificationExpiry -refreshTokenExpiry -resetPasswordExpiry"
     );
     if (!user) {
       throw new ApiError(404, "User not found.");
     }
-    req.user = user;
+    req.user = user as decodedUser;
     next();
   }
 );
