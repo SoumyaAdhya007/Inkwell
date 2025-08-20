45cba0c9246c0f8c680fd2a3b109ca2df9ee2f46
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-08-20 22:43:16 +0530

    feat(categories/routes): add routes for getCategories, getCategoryById, createCategory, updateCategory, and deleteCategory

---
+++ src/categories/routes/category.routes.ts
@@ -0,0 +1,44 @@
+import { Router } from "express";
+import validateReq from "../../middlewares/validationRequest.middlewares";
+import {
+  createCategoryValidator,
+  deleteCategoryByIdValidator,
+  getCategoryByIdValidator,
+  updateCategoryByIdValidator,
+} from "../validators/category.validators";
+import {
+  createCategory,
+  deleteCategoryById,
+  getAllCategories,
+  getCategoryById,
+  updateCategoryById,
+} from "../controllers/category.controllers";
+import authCheck from "../../middlewares/auth.middlewares";
+import adminCheck from "../../middlewares/admin.middleware";
+import apiKeyCheck from "../../middlewares/apiKey.middlewares";
+
+const router = Router();
+
+router.route("/").get(authCheck, apiKeyCheck, getAllCategories);
+
+router
+  .route("/:id")
+  .get(
+    authCheck,
+    apiKeyCheck,
+    validateReq(getCategoryByIdValidator),
+    getCategoryById
+  );
+
+router.use(authCheck, adminCheck, apiKeyCheck);
+router.route("/").post(validateReq(createCategoryValidator), createCategory);
+
+router
+  .route("/:id")
+  .put(validateReq(updateCategoryByIdValidator), updateCategoryById);
+
+router
+  .route("/:id")
+  .delete(validateReq(deleteCategoryByIdValidator), deleteCategoryById);
+
+export default router;
