5b8ae882559cd072810d90419bb305475da2f3fc
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-08-20 23:29:23 +0530

    feat(utils/constants): add api key and post constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,4 +5,27 @@ const userRolesEnum = {
 
 const AvailableUserRoles = Object.values(userRolesEnum);
 
-export { userRolesEnum, AvailableUserRoles };
+// api key status constants
+const apikeyStatusesEnum = {
+  Active: "ACTIVE",
+  Inactive: "INACTIVE",
+  Revoked: "REVOKED",
+};
+const AvailableApiKeyStatuses = Object.values(apikeyStatusesEnum);
+
+// post status constants
+const postStatusesEnum = {
+  Approved: "APPROVED",
+  Pending: "PENDING",
+  Rejected: "REJECTED",
+};
+const AvailablePostStatuses = Object.values(postStatusesEnum);
+
+export {
+  userRolesEnum,
+  AvailableUserRoles,
+  apikeyStatusesEnum,
+  AvailableApiKeyStatuses,
+  postStatusesEnum,
+  AvailablePostStatuses,
+};
