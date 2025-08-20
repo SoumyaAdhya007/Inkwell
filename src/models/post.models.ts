63a3c06e47c72e84d68e001c7597d32b049c6cfa
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-08-20 23:19:48 +0530

    feat(models/post): define mongoose schema and model for post

---
+++ src/models/post.models.ts
@@ -0,0 +1,56 @@
+import mongoose, { Document } from "mongoose";
+import { AvailablePostStatuses, postStatusesEnum } from "../utils/constants";
+
+interface IPost extends Document {
+  userId: mongoose.Schema.Types.ObjectId;
+  title: string;
+  content: string;
+  category: mongoose.Schema.Types.ObjectId;
+  publicationDate: Date;
+  tags: [string];
+  status: string;
+}
+
+const postSchema = new mongoose.Schema<IPost>(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      require: true,
+    },
+    content: {
+      type: String,
+      require: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    publicationDate: {
+      type: Date,
+      default: Date.now,
+      require: true,
+    },
+    tags: [
+      {
+        type: String,
+        require: true,
+      },
+    ],
+    status: {
+      type: String,
+      enum: AvailablePostStatuses,
+      default: postStatusesEnum.Pending,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model<IPost>("Post", postSchema);
+
+export default Post;
