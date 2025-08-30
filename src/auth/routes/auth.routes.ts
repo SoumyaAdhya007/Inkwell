888b47924c2a1c1fdede512490438d716377932d
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-08-30 23:27:40 +0530

    feat(auth/routes): add loginLimiter for login route and apiKeyLimiter for me route

--- src/auth/routes/auth.routes.ts
+++ src/auth/routes/auth.routes.ts
@@ -13,6 +13,8 @@ import {
   loginValidator,
 } from "../validators/auth.validators";
 import authCheck from "../../middlewares/auth.middlewares";
+import apiKeyCheck from "../../middlewares/apiKey.middlewares";
+import { apiKeyLimiter, loginLimiter } from "../../utils/rateLimiter";
 const router = Router();
 
 router.route("/register").post(validateReq(registerValidator), register);
@@ -20,10 +22,12 @@ router.route("/register").post(validateReq(registerValidator), register);
 router
   .route("/verify-email/:token")
   .get(validateReq(verifyEmailValidator), verifyEmail);
-router.route("/login").post(validateReq(loginValidator), login);
+router.route("/login").post(loginLimiter, validateReq(loginValidator), login);
 
 router.route("/api-key").get(authCheck, createApiKey);
 
-router.route("/me").get(authCheck, getMe);
+router.use(authCheck, apiKeyCheck, apiKeyLimiter);
+
+router.route("/me").get(getMe);
 
 export default router;
