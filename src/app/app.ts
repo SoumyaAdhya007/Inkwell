4ce5ec6b95bc69340e33fb77cea1808203b22afe
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-09-11 00:11:02 +0530

    feat(app): add root '/' route to show project info

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import errorMiddleware from "../middlewares/error.middlewares";
 import AdminRouter from "../admin/routes/admin.routes";
@@ -6,12 +6,25 @@ import AuthRouter from "../auth/routes/auth.routes";
 import CategoriesRouter from "../categories/routes/category.routes";
 import PostsRouter from "../posts/routes/post.routes";
 import HealthCheckRouter from "../healthCheck/routes/healthCheck.routes";
+import ApiResponse from "../utils/ApiResponse";
+
 function app(): Express {
   const app: Express = express();
 
   app.use(cookieParser());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  app.get("/", (req: Request, res: Response) => {
+    res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          `Inkwell is a blog publishing platform where user posts must be approved by an admin before being published.`
+        )
+      );
+  });
 
   app.use("/api/v1/admin", AdminRouter);
   app.use("/api/v1/auth", AuthRouter);
